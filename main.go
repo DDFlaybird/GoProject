fad89ac1ddd8146431f877b510940b6562d27a98
杜腾飞 <dutengfei@travelsky.com>
2020-08-25 23:51:38 +0800

    “更新文件名称为小写”

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	fmt.Println("***************TypeCoversion*****************")
+	fmt.Println("***************typecoversion*****************")
 	ac := 3
 	bc := 10
 	a := "100"
@@ -17,14 +17,14 @@ func main() {
 	fmt.Println(typecoversion.StrCovert(a))
 	fmt.Println(typecoversion.FmtIntConvertExample(b))
 	fmt.Println(bc % ac)
-	fmt.Println("***************TypeCoversion*****************")
+	fmt.Println("***************typecoversion*****************")
 	// 操作符
 	operator.BitwiseOperatorTestCase()
 	// 流程控制
 	fmt.Println("***************FLowControl*****************")
 	flowcontrol.AnthorSimpleIfExample(22)
 	flowcontrol.ElifSimpleExample(52)
-	//FlowControl.ForSimpleExample()
+	//flowcontrol.ForSimpleExample()
 	flowcontrol.EventNumberExample(22)
 	flowcontrol.SumExample(12)
 	flowcontrol.TimesRangeExample(100, 9)
